b21d4d75131b80b832984712b898cc18f999c1c2
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2019-05-01 05:28:55 -0400

    partial run of tr0

--- visualize_reconstructions.py
+++ visualize_reconstructions.py
@@ -1,23 +1,26 @@
 from models import Autoencoder
 from set_up_translation import *
 import torch.nn as nn
+import os
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 autoencoder = torch.load(os.path.join(fixed_vars['autoencoder_directory'], "autoencoder.model"),
-                         map_location=fixed_vars['device'])
+                         map_location=device)
 
 autoencoder_objects = get_autoencoder_objects()
 # example_sentence = "Hey Chris, here's an example sentence that I'm hoping to recreate."
 # example_sentence = "This is an image of a bird, which is an animal that I quite like."
 # example_sentence = "Horrible"
 
-# example_sentence = "A brown dog is drinking water out of a bowl."
-example_sentence = "Hey Chris, I think that this work is pretty good."
+example_sentence = "A brown dog is drinking water out of a bowl."
+# example_sentence = "Hey Chris, I think that this work is pretty good."
 
 def test_reconstruction(utterance):
     e = autoencoder_objects['english_bert_tokenizer']
     s = autoencoder_objects['src_field']
-    src = autoencoder_objects['trg_field'].process([s.preprocess(e.tokenize(utterance))]).to(fixed_vars['device'])
-    output = autoencoder(src, src, 1)
+    src = autoencoder_objects['trg_field'].process([s.preprocess(e.tokenize(utterance))]).to(device)
+    output = autoencoder(src, src, 1).to(device)
     _, best_guess = torch.max(output, dim=2)
     print(best_guess)
 
