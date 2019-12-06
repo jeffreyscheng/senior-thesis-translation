cee9267f3aea3e8531ce23558bf672876bd4fb43
Jeffrey Cheng <jeff.s.cheng@gmail.com>
2019-12-06 03:46:02 -0500

    added L2

--- run_translator.py
+++ run_translator.py
@@ -11,7 +11,7 @@ import time
 loss_df_list = []
 translator_objects = get_translation_objects()
 
-proportions = [0.1]
+proportions = [1]
 # proportions = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0]
 for proportion_of_data in proportions:
     print("Began: ", proportion_of_data)
@@ -29,7 +29,9 @@ for proportion_of_data in proportions:
 
     print("created new encoder + decoder")
     translator = Translator(bert_encoder, autoencoder.decoder, fixed_vars['device']).to(fixed_vars['device'])
-    translator_optimizer = optim.Adam(list(translator.decoder.parameters()) + list(translator.fc1.parameters()) + list(translator.fc2.parameters()), lr=translator_hyperparameters['learning_rate'])
+    translator_optimizer = optim.Adam(list(translator.decoder.parameters()) + list(translator.fc1.parameters()) + list(translator.fc2.parameters()),
+                                      lr=translator_hyperparameters['learning_rate'],
+                                      weight_decay=10 ** (-5))
     PAD_IDX = 0
     criterion = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
     print("Initialized all torch objects and models.  Now training.")
